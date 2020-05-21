38117bff621c4d08fa87470b466b70076053f05b
Gregory Russell <gfr@google.com>
2020-05-21 10:21:18 -0400

    add copy and delete actions

--- ops/actions.go
+++ ops/actions.go
@@ -21,7 +21,7 @@ import (
 )
 
 func newStateFunc(detail string) ActionFunc {
-	return func(ctx context.Context, j tracker.Job) *Outcome {
+	return func(ctx context.Context, j tracker.Job, stateChangeTime time.Time) *Outcome {
 		return Success(j, detail)
 	}
 }
@@ -46,13 +46,23 @@ func NewStandardMonitor(ctx context.Context, config cloud.BQConfig, tk *tracker.
 	m.AddAction(tracker.Deduplicating,
 		nil,
 		dedupFunc,
-		tracker.Complete,
+		tracker.Copying,
 		"Deduplicating")
+	m.AddAction(tracker.Copying,
+		nil,
+		copyFunc,
+		tracker.Cleaning,
+		"Copying")
+	m.AddAction(tracker.Cleaning,
+		nil,
+		deleteFunc,
+		tracker.Complete,
+		"Cleaning")
 	return m, nil
 }
 
 // Waits for bqjob to complete, handles backoff and job updates.
-// Returns non-nil status if successful.
+// If Outcome is Success, status will be non-nil, and non-error.
 func waitAndCheck(ctx context.Context, bqJob bqiface.Job, j tracker.Job, label string) (*bigquery.JobStatus, *Outcome) {
 	status, err := bqJob.Wait(ctx)
 	if err != nil {
@@ -83,6 +93,9 @@ func waitAndCheck(ctx context.Context, bqJob bqiface.Job, j tracker.Job, label s
 	if status.Err() != nil {
 		err := status.Err()
 		log.Println(label, err)
+		// Copy {Location: "partition_modifications_per_column_partitioned_table.long";
+		// Message: "Quota exceeded: Your table exceeded quota for Number of partition modifications to a column partitioned table. For more information, see https://cloud.google.com/bigquery/troubleshooting-errors"; Reason: "quotaExceeded"}
+		// limited to a toal of 5,000 partition modifications per day
 		metrics.WarningCount.WithLabelValues(
 			j.Experiment, j.Datatype,
 			label+"UnknownStatusError").Inc()
@@ -94,11 +107,10 @@ func waitAndCheck(ctx context.Context, bqJob bqiface.Job, j tracker.Job, label s
 }
 
 // TODO improve test coverage?
-func dedupFunc(ctx context.Context, j tracker.Job) *Outcome {
-	start := time.Now()
+func dedupFunc(ctx context.Context, j tracker.Job, stateChangeTime time.Time) *Outcome {
 	// This is the delay since entering the dedup state, due to monitor delay
 	// and retries.
-	// delay := time.Since(s.LastStateChangeTime()).Round(time.Minute)
+	delay := time.Since(stateChangeTime).Round(time.Minute)
 
 	var bqJob bqiface.Job
 	var msg string
@@ -119,18 +131,16 @@ func dedupFunc(ctx context.Context, j tracker.Job) *Outcome {
 	if !errors.Is(outcome, IsDone) {
 		return outcome
 	}
-	if status == nil {
-		// Nil status means the job failed.
-		return Failure(j, errors.New("nil status"), "-")
-	}
 
 	// Dedup job was successful.  Handle the statistics, metrics, tracker update.
-	switch details := status.Statistics.Details.(type) {
+	stats := status.Statistics
+	switch details := stats.Details.(type) {
 	case *bigquery.QueryStatistics:
+		opTime := stats.EndTime.Sub(stats.StartTime)
 		metrics.QueryCostHistogram.WithLabelValues(j.Datatype, "dedup").Observe(float64(details.SlotMillis) / 1000.0)
-		msg = fmt.Sprintf("Dedup took %s (after xxx waiting), %5.2f Slot Minutes, %d Rows affected, %d MB Processed, %d MB Billed",
-			time.Since(start).Round(100*time.Millisecond).String(),
-			//delay,
+		msg = fmt.Sprintf("Dedup took %s (after %v waiting), %5.2f Slot Minutes, %d Rows affected, %d MB Processed, %d MB Billed",
+			opTime.Round(100*time.Millisecond),
+			delay,
 			float64(details.SlotMillis)/60000, details.NumDMLAffectedRows,
 			details.TotalBytesProcessed/1000000, details.TotalBytesBilled/1000000)
 		log.Println(msg)
@@ -139,6 +149,64 @@ func dedupFunc(ctx context.Context, j tracker.Job) *Outcome {
 		log.Printf("Could not convert to QueryStatistics: %+v\n", status.Statistics.Details)
 		msg = "Could not convert Detail to QueryStatistics"
 	}
+	return Success(j, msg)
+}
 
+// TODO This is costly.  Consider using decorated table delete.
+// TODO improve test coverage?
+func deleteFunc(ctx context.Context, j tracker.Job, stateChangeTime time.Time) *Outcome {
+	// TODO pass in the JobWithTarget, and get the base from the target.
+	qp, err := bq.NewQuerier(j, os.Getenv("PROJECT"))
+	if err != nil {
+		log.Println(err)
+		// This terminates this job.
+		return Failure(j, err, "-")
+	}
+	err = qp.DeleteTmp(ctx)
+	if err != nil {
+		log.Println(err)
+		// Try again soon.
+		return Retry(j, err, "-")
+	}
+
+	// TODO - add elapsed time to message.
+	return Success(j, "Successfully deleted partition")
+}
+
+// TODO improve test coverage?
+func copyFunc(ctx context.Context, j tracker.Job, stateChangeTime time.Time) *Outcome {
+	// This is the delay since entering the dedup state, due to monitor delay
+	// and retries.
+	delay := time.Since(stateChangeTime).Round(time.Minute)
+
+	var bqJob bqiface.Job
+	// TODO pass in the JobWithTarget, and get the base from the target.
+	qp, err := bq.NewQuerier(j, os.Getenv("PROJECT"))
+	if err != nil {
+		log.Println(err)
+		// This terminates this job.
+		return Failure(j, err, "-")
+	}
+	bqJob, err = qp.CopyToRaw(ctx, false)
+	if err != nil {
+		log.Println(err)
+		// Try again soon.
+		return Retry(j, err, "-")
+	}
+	status, outcome := waitAndCheck(ctx, bqJob, j, "Copy")
+	if !errors.Is(outcome, IsDone) {
+		return outcome
+	}
+
+	var msg string
+	stats := status.Statistics
+	if stats != nil {
+		opTime := stats.EndTime.Sub(stats.StartTime)
+		msg = fmt.Sprintf("Copy took %s (after %s waiting), %d MB Processed",
+			opTime.Round(100*time.Millisecond),
+			delay,
+			stats.TotalBytesProcessed/1000000)
+		log.Println(msg)
+	}
 	return Success(j, msg)
 }
--- ops/actions_test.go
+++ ops/actions_test.go
@@ -2,19 +2,41 @@ package ops_test
 
 import (
 	"context"
+	"log"
 	"testing"
 	"time"
 
-	"github.com/m-lab/etl-gardener/cloud"
-	"github.com/m-lab/etl-gardener/ops"
-	"github.com/m-lab/etl-gardener/tracker"
 	"github.com/m-lab/go/logx"
 	"github.com/m-lab/go/osx"
 	"github.com/m-lab/go/rtx"
+
+	"github.com/m-lab/etl-gardener/cloud"
+	"github.com/m-lab/etl-gardener/cloud/bq"
+	"github.com/m-lab/etl-gardener/ops"
+	"github.com/m-lab/etl-gardener/tracker"
 )
 
+func xTestHack(t *testing.T) {
+	ctx := context.Background()
+
+	date, err := time.Parse("20060102", "20200201")
+	rtx.Must(err, "parse date")
+	job := tracker.NewJob("fake", "ndt", "tcpinfo", date)
+	q, err := bq.NewQuerier(job, "mlab-sandbox")
+	rtx.Must(err, "querier")
+	j, err := q.CopyToRaw(context.Background(), false)
+
+	rtx.Must(err, "copy")
+	s, err := j.Wait(ctx)
+	//var ss bigquery.JobStatus
+	rtx.Must(err, "wait")
+	log.Printf("%+v\n", s)
+	log.Printf("%+v\n", s.Statistics)
+	t.Error()
+}
+
 // TODO consider rewriting to use a go/cloud/bqfake client.  This is a fair
-// bit of work, though.
+// bit of work, though.  Might be practical to improve test coverage.
 func TestStandardMonitor(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping test that uses BQ backend")
@@ -47,12 +69,19 @@ func TestStandardMonitor(t *testing.T) {
 		tracker.ParseComplete,
 		"Parsing")
 
+	// Delete action doesn't from travis...
+	m.AddAction(tracker.Cleaning,
+		nil,
+		newStateFunc("-"),
+		tracker.Complete,
+		"Cleaning")
+
 	// The real dedup action should fail on unknown datatype.
 	go m.Watch(ctx, 50*time.Millisecond)
 
 	failTime := time.Now().Add(30 * time.Second)
 
-	for time.Now().Before(failTime) && (tk.NumJobs() > 2 || tk.NumFailed() < 2) {
+	for time.Now().Before(failTime) && tk.NumFailed() < 3 && (tk.NumJobs() > 2 || tk.NumFailed() < 2) {
 		time.Sleep(time.Millisecond)
 	}
 	if tk.NumFailed() != 2 {
